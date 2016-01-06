c4331fa9458c39f3dea36d65fcfce6a4a0f74856
hrendalf <hrendalf@users.noreply.github.com>
2016-01-05 21:10:20 -0500

    moved files

---
+++ js/happyend.js
@@ -0,0 +1,22 @@
+happyendings = [
+
+    {
+        author: "Dumas, Alexandre",
+        title: "The Three Musketeers",
+        quote: "M. Bonacieux lived on very quietly, wholly ignorant of what had become of his wife, and caring very little about it. One day he had the imprudence to recall himself to the memory of the cardinal. The cardinal had him informed that he would provide for him so that he should never want for anything in future. In fact, M. Bonacieux, having left his house at seven o'clock in the evening to go to the Louvre, never appeared again in the Rue des Fossoyeurs; the opinion of those who seemed to be best informed was that he was fed and lodged in some royal castle, at the expense of his generous Eminence."
+    },
+
+    {
+        author: "Nikolai Gogol",
+        title: "Dead Souls",
+        quote: "Long did our hero ponder the scheme; until at length the slumber which had, these four hours past, been holding the rest of the household in its embraces enfolded also Chichikov, and he sank into oblivion."
+    },
+
+    {
+        author: "1",
+        title: "2",
+        quote: "3"
+    },
+
+
+]
