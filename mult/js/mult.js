b056921ed3703ec004da1b183eb9cef0213f9f7c
hrendalf <hrendalf@users.noreply.github.com>
2020-04-12 17:24:20 -0400

    fit result into one line, wire initial question

--- mult/js/mult.js
+++ mult/js/mult.js
@@ -2,14 +2,28 @@ function randomIntBetween(min, max) { // inclusive
   return Math.floor(min + Math.random() * (max - min + 1));
 }
 
+function hideNumbers(text) {
+    return text.replace(/[0-9]/g, "?")
+}
+
+function rollFirst(taskTarget, solveTarget) {
+    rollExact(taskTarget, solveTarget, 1234, 567);
+}
+
 function roll(taskTarget, solveTarget) {
     var a = randomIntBetween(100, 2999);
     var b = randomIntBetween(10, 999);
+    rollExact(taskTarget, solveTarget, a, b);
+}
 
-    taskTarget.text(a + " x " + b);
-    taskTarget.data("result", a + " x " + b + " = " + (a*b));
+function rollExact(taskTarget, solveTarget, a, b) {
+    var question = a + " x " + b + " = " + hideNumbers(""+ (a*b));
+    var answer = a + " x " + b + " = " + (a*b);
+
+    taskTarget.text(question);
+    taskTarget.data("answer", answer);
 }
 
 function showMe(taskTarget, solveTarget) {
-    taskTarget.text(taskTarget.data("result"));   
+    taskTarget.text(taskTarget.data("answer"));
 }
