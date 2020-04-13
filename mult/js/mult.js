e0ff433fe736c3aa2be6425bda4e8ab835d49522
hrendalf <hrendalf@users.noreply.github.com>
2020-04-12 20:30:27 -0400

    write solution

--- mult/js/mult.js
+++ mult/js/mult.js
@@ -12,7 +12,7 @@ function rollFirst(taskTarget, solveTarget) {
 
 function roll(taskTarget, solveTarget) {
     var a = randomIntBetween(100, 2999);
-    var b = randomIntBetween(10, 333);
+    var b = randomIntBetween(100, 333);
     rollExact(taskTarget, solveTarget, a, b);
 }
 
@@ -22,8 +22,35 @@ function rollExact(taskTarget, solveTarget, a, b) {
 
     taskTarget.text(question);
     taskTarget.data("answer", answer);
+    
+    var solve = a + "\n";
+    solve = solve + b + "\n";
+    solve = solve + "--------" + "\n";
+    
+    var left = b;
+    var space = "";
+    var sum = 0;
+    var m = 1;
+    
+    while (left > 0) { 
+        var last = left % 10;
+        left = (left - last) / 10;
+    
+        solve = solve + (a * last) + space + "\n";
+        space = space + " ";
+        
+        sum = sum + (a * last * m);
+        m = m * 10;
+    }
+    solve = solve + "--------" + "\n";
+    solve = solve + sum;
+    
+    
+    solveTarget.text(hideNumbers(solve));
+    solveTarget.data("answer", solve);
 }
 
 function showMe(taskTarget, solveTarget) {
     taskTarget.text(taskTarget.data("answer"));
+    solveTarget.text(solveTarget.data("answer"));
 }
